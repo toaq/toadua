7032b3e25463e1968632f4ae2f0cfdc379d0c2eb
uakci <uakci@uakci.pl>
2022-02-15 04:02:57 +0100

    improve flake; add nixos module

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
     flake-utils.url = "github:numtide/flake-utils";
     npmlock2nix = {
       url = "github:nix-community/npmlock2nix/master";
@@ -9,18 +9,57 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, npmlock2nix, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+    (flake-utils.lib.eachDefaultSystem (system:
       let
-        package = dev: (import npmlock2nix {
-          pkgs = nixpkgs.legacyPackages.${system};
-        }).build {
-          src = ./.;
-          buildCommands = [ (if dev then "npm run dev" else "npm run prod") ];
-          installPhase = "cp -r . $out";
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
+        nodejs = pkgs.nodejs-14_x;
+        package = { dev }:
+          (import npmlock2nix { inherit pkgs; }).build {
+            src = ./.;
+            inherit nodejs;
+            buildCommands = [ (if dev then "npm run dev" else "npm run prod") ];
+            installPhase = ''
+              cp -r . $out
+              mkdir $out/bin
+              tee >> $out/bin/toadua <<EOF
+                cd $out
+                ${nodejs}/bin/node ./core/server.js \$@
+              EOF
+              chmod +x $out/bin/toadua
+            '';
+          };
+        toadua = package { dev = false; };
+        toaduaDev = package { dev = true; };
       in {
-        packages.toadua = package false;
-        packages.toadua-dev = package true;
-        defaultPackage = package false;
-      });
+        packages = { inherit toadua toaduaDev; };
+        defaultPackage = toadua;
+        devShell = pkgs.mkShell { buildInputs = [ nodejs ]; };
+      })) // {
+        nixosModule = { pkgs, lib, config, inputs, system, ... }:
+          let inherit (config.services.toadua) enabled package port dataDir;
+          in with lib; {
+            options.services.toadua = {
+              enabled = mkEnableOption "toadua";
+              package = mkOption {
+                default = self.defaultPackage.${system};
+                type = types.package;
+              };
+              port = mkOption { type = types.port; };
+              dataDir = mkOption { type = types.path; };
+            };
+            config.systemd.services.toadua = {
+              inherit enabled;
+              description = "Toaq Dictionary";
+              wantedBy = [ "multi-user.target" ];
+              wants = [ "network-online.target" ];
+              script = strings.concatStringsSep " " ([
+                "${pkgs.nodejs-16_x}/bin/node"
+                "${package}/core/server.js"
+                "--data-directory ${dataDir}"
+              ] ++ (lib.optionals (port != null)
+                [ "--port ${toString port}" ]));
+              serviceConfig.WorkingDirectory = dataDir;
+            };
+          };
+      };
 }
