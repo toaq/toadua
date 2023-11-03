c3fe38e70fd24372a61c5a7560ab30b1e3b73a8f
Lynn <lynn@foldr.moe>
2023-11-03 01:02:03 +0100

    Run Prettier

--- frontend/build.js
+++ frontend/build.js
@@ -1,12 +1,14 @@
-import vuePlugin from 'esbuild-plugin-vue3'
-import * as esbuild from 'esbuild'
+import vuePlugin from 'esbuild-plugin-vue3';
+import * as esbuild from 'esbuild';
 import fs from 'fs';
 
-esbuild.build({
-    entryPoints: ['frontend.ts'],
-    bundle: true,
-    outfile: 'dist/build.js',
-    plugins: [vuePlugin()],
-}).then(() => {
-    fs.copyFileSync('index.html', 'dist/index.html');
-});
+esbuild
+	.build({
+		entryPoints: ['frontend.ts'],
+		bundle: true,
+		outfile: 'dist/build.js',
+		plugins: [vuePlugin()],
+	})
+	.then(() => {
+		fs.copyFileSync('index.html', 'dist/index.html');
+	});
