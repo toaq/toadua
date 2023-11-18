b7dd6fc01bd419cc6e6c292cbda3ee7e24c0df0e
Lynn <lynn@foldr.moe>
2023-11-18 23:15:45 +0100

    Silly trick to align date baseline

--- frontend/App.vue
+++ frontend/App.vue
@@ -64,7 +64,9 @@
 					:title="full_date(new Date(result.date))"
 					:href="'##' + result.id"
 					@click="navigate('#' + result.id)"
-					>{{ pretty_date(new Date(result.date)) }}</a
+				>
+					<span style="font-size: 24px">&ZeroWidthSpace;</span>
+					{{ pretty_date(new Date(result.date)) }}</a
 				>
 				<h2>
 					<a
